a811c652c17e146298b79e56273f658f3619be61
BanChaM <6611850018@mut.ac.th>
2024-07-12 17:24:19 +0700

    mahanakorn

--- login.php
+++ login.php
@@ -5,9 +5,3 @@ echo <h1>"User"</h1> ;
 echo <h1>"Password"</h1> ; 
 echo <h1>"</table>"</h1> ; 
 ?>
-=======
-echo<h1>"User"</h1> ; 
-echo<h1>"Password"</h1> ; 
-echo<h1>"Yahoo"</h1> ; 
-?>
->>>>>>> 44c4e532c3679b30f45d4e9d06eaa2008b57ff60
---
+++ mylocation.php
@@ -0,0 +1,6 @@
+<?php 
+echo <h1>"<table>"</h1> ; 
+echo <h1>"Nongjok"</h1> ; 
+echo <h1>"Naja"</h1> ; 
+echo <h1>"</table>"</h1> ; 
+?>
